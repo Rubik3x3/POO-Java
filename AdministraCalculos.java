534999a6ce1ca603f84208d0a598408c78fe1f52
Franco Salvador Talarico <84205093+Rubik3x3@users.noreply.github.com>
2025-03-18 09:21:33 -0300

    Update AdministraCalculos.java

--- AdministraCalculos.java
+++ AdministraCalculos.java
@@ -8,12 +8,20 @@ public class AdministraCalculo {
 		return num1+num2;
 	}
 	
+	public static int suma(int num1, int num2, int num3) {
+		return num1+num2+num3;
+	}
+	
 	public static double suma(double num1, int num2) {
 		return num1+num2;
 	}
 	
 	public static double suma(int num1, double num2) {
 		return num1+num2;
+	}
+	
+	public static double suma(double num1) {
+		return num1;
 	}
 	
 	public static double suma(double num1, double num2) {
@@ -29,12 +37,15 @@ public class AdministraCalculo {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(suma(1, 5)); // Int Int
-		System.out.println(suma(1.5, 5)); // Double Int
+		System.out.println(suma(2, 7)); // Int Int
+		System.out.println(suma(2, 7, 7));// Int Int Int
+		System.out.println(suma(2.83, 7)); // Double Int
 		System.out.println(suma(1, 5.3)); // Int Double
 		System.out.println(suma(1.2, 5.8)); // Double Double
 		System.out.println(suma("1",5)); // String Int
 		System.out.println(suma(1,"5")); // Int String
+		System.out.println(suma(2.5)); // Double
+		System.out.println(suma("El doble del n°: ",7)); // String Int
 	}
 
 }
