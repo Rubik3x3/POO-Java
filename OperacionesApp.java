e6a54c8533ab81460f4212fc025fee8bf6a68e14
Franco Salvador Talarico <84205093+Rubik3x3@users.noreply.github.com>
2025-03-21 08:30:52 -0300

    Create OperacionesApp.java

---
+++ OperacionesApp.java
@@ -0,0 +1,20 @@
+
+public class OperacionesApp {
+
+	public static void main(String[] args) {
+		double n1 = 25;
+		double n2 = 4;
+		
+		double suma = n1 + n2;
+		double resta = n1 - n2;
+		double multiplicacion = n1 * n2;
+		double division = n1 / n2;
+		
+		System.out.println("Números: "+n1+", "+n2);
+		System.out.println("Resultado de la suma: "+suma);
+		System.out.println("Resultado de la resta: "+resta);
+		System.out.println("Resultado de la multiplicacion: "+multiplicacion);
+		System.out.println("Resultado de la division: "+division);
+	}
+
+}
